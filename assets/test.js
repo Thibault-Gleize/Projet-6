d1275056bca3135abc08a3629f73ea78cf032b55
Thibault Gleize <tibo.gleize@gmail.com>
2024-10-15 20:48:07 +0300

    Finition de la partie login pour récupérer le token et ajout de la partie Logged afin d'afficher les éléments de la page en mode édition

--- assets/test.js
+++ assets/test.js
@@ -42,30 +42,47 @@ formLogin.addEventListener("submit", async function (event) {
     }
 })
 
-submit()
+
+
+
+
+
+
+
+
 
 
 /* 
-1) Lors de l'appuie sur le bouton viens prendre les values
-dans e-mail et mdp 
+export function userData (event) {
+    const log = {
+        email: event.target.querySelector("[name=email]").value,
+        password : event.target.querySelector("[name=password]").value
+    }
+    // Création Charge utile 
+    const chargeUtile = JSON.stringify(log)
+    const response = fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: chargeUtile
+    })
+    const token = response.then(response => response.json()
+    .then(response => response.token))
+    return token
+}
 
-2) Effectue ensuite la charge utile et la reponse
-
-3) return la reponse pour ensuite vérifier si status OK
-et enregistrement du token
-PS : check keep-alive et timeout dans header POSTMAN
-
-4) renvoie vers la page index qui permet les modifications
-
-5) Promesse  .catch
+const submit = formLogin.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    userData(event)
+    const x = await userData(event)
+    if (x !== undefined) {
+        console.log(x);
+    }
+    else {
+    const champEmail = document.querySelector(".form-login");
+    const wrongPwd = document.createElement("p")
+    wrongPwd.innerText = "E-mail ou mot de passe incorrect"
+    champEmail.appendChild(wrongPwd)
+    }
+})
 */
 
-
-/* Partie Modification de la page index une fois connecté 
-
-1) Une fois connecté = token enregistré dans x dans submit. 
-
-2) Autre fichier Connected qui permet de modifier le HTML
-
-
-*/
